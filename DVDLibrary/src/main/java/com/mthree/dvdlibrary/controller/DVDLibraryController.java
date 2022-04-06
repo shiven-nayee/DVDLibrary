691644c911b5a3b5e8ddc886cc9a2667a3a21008
Jeonghoon Lee <hoon22.lee@gmail.com>
2022-04-06 14:15:58 -0400

    updated controller

--- DVDLibrary/src/main/java/com/mthree/dvdlibrary/controller/DVDLibraryController.java
+++ DVDLibrary/src/main/java/com/mthree/dvdlibrary/controller/DVDLibraryController.java
@@ -4,11 +4,14 @@
  */
 package com.mthree.dvdlibrary.controller;
 
+import com.mthree.dvdlibrary.dao.DVDLibraryDao;
 import com.mthree.dvdlibrary.dao.DVDLibraryDaoException;
+import com.mthree.dvdlibrary.dao.DVDLibraryDaoFileImpl;
 import com.mthree.dvdlibrary.dto.DVD;
 import com.mthree.dvdlibrary.ui.DVDLibraryView;
 import com.mthree.dvdlibrary.ui.UserIO;
 import com.mthree.dvdlibrary.ui.UserIOConsoleImpl;
+import java.util.List;
 
 /**
  *
@@ -18,6 +21,7 @@ public class DVDLibraryController {
 
     // for testing
     UserIO io = new UserIOConsoleImpl();
+    DVDLibraryDao dao = new DVDLibraryDaoFileImpl();
     DVDLibraryView view = new DVDLibraryView();
 
     public void run() {
@@ -68,36 +72,49 @@ public class DVDLibraryController {
     void addDVD() throws DVDLibraryDaoException {
         view.displayCreateDVDBanner();
         DVD newDVD = view.getNewDVDInfo();
+        dao.addDVD(newDVD);
         view.displayCreateSuccessBanner();
     }
 
     void removeDVD() {
         view.displayRemoveDVDBanner();
         String title = view.getDVDTitle();
-//        view.displayRemoveResult(removedDVD);
+        DVD removedDVD = dao.removeDVD(title);
+        view.displayRemoveResult(removedDVD);
     }
 
     void editDVDInformation() {
         view.displayEditDVDBanner();
-        String title = view.getDVDTitle();
-        view.displayEditDVDMenu();
+//        String title = view.getDVDTitle();
+//        DVD dvdForEditing = dao.getDVD(title);
+//
+//        view.displayEditDVDMenu();
+//        // prompt for options
+//
 //        view.displayEditResult(editedDVD);
     }
 
     void listDVDs() {
         view.displayDisplayAllBanner();
-//        List<DVD> dvdList = dao.getAllDVDs();
-//        view.displayDVDList(dvdList);
+        List<DVD> dvdList = dao.listDVDs();
+        view.displayDVDList(dvdList);
     }
 
     void displayDVDInformation() {
         view.displayDisplayDVDBanner();
         String title = view.getDVDTitle();
+        DVD dvd = dao.getDVD(title);
+        view.displayDVD(dvd);
     }
 
     void searchDVDByTitle() {
         view.displaySearchDVDBanner();
         String title = view.getDVDTitle();
+        if (dao.searchDVD(title)) {
+            System.out.println("Exist");
+        } else {
+            System.out.println("We don't have it");
+        }
     }
 
     private void unknownCommand() {
