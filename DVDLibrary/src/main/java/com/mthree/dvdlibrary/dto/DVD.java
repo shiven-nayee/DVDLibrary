ea897a323b08fdd05f27a430c7e6e68b0688d06b
Jeonghoon Lee <hoon22.lee@gmail.com>
2022-04-06 10:52:35 -0400

    completed DVD class

--- DVDLibrary/src/main/java/com/mthree/dvdlibrary/dto/DVD.java
+++ DVDLibrary/src/main/java/com/mthree/dvdlibrary/dto/DVD.java
@@ -4,10 +4,71 @@
  */
 package com.mthree.dvdlibrary.dto;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  *
  * @author Jeonghoon
  */
 public class DVD {
 
+    private String title;           // KEY
+    private Date releaseDate;
+    private String rating;
+    private String directorName;
+    private String studio;
+    private String userRating;
+
+    public DVD(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getDirectorName() {
+        return directorName;
+    }
+
+    public void setDirectorName(String directorName) {
+        this.directorName = directorName;
+    }
+
+    public String getStudio() {
+        return studio;
+    }
+
+    public void setStudio(String studio) {
+        this.studio = studio;
+    }
+
+    public String getUserRating() {
+        return userRating;
+    }
+
+    public void setUserRating(String userRating) {
+        this.userRating = userRating;
+    }
 }
